436c8d985e16eef6e5e17a906ab9c6f663c92a80
stealthunit1337 <stealthunit123@gmail.com>
2022-08-04 14:16:21 +0300

    123

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.wrapper about page
+  .wrapper-1 123
+
+</template>
+
+<style lang="scss">
+.wrapper {
+  &-1 {
+    color: $hover
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+import pugPlugin from "vite-plugin-pug"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), pugPlugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/_variables.scss"; @import "@/assets/scss/_mixins.scss";`
+
+
+      }
+    }
+  },
+})
