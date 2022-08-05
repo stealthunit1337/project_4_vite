1370ac950d55016d08713cca6b32fc3e766e0201
stealthunit <stealthunit123@gmail.com>
2022-08-06 01:04:16 +0400

    accordion, carousel

---
+++ src/components/MyCarousel.vue
@@ -0,0 +1,124 @@
+<template>
+	<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+		<div class="carousel-indicators">
+			<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+				aria-current="true" aria-label="Slide 1"></button>
+			<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+			<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+		</div>
+		<div class="carousel-inner">
+
+			<div class="carousel-item active" data-bs-interval="10000">
+				<div class="carousel-wrapper">
+					<div class="carousel-caption">
+						<h5>Second slide label</h5>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur corrupti doloremque
+							ullam deleniti, voluptatem dolore quaerat, totam quis repellendus magnam expedita fugiat iusto
+							soluta. Perferendis quibusdam non veritatis voluptatem.
+							Quod inventore dolorum tempore magnam ex aut earum eaque deleniti alias asperiores quaerat nostrum
+							consectetur suscipit dolores, sed ullam fugit quos laborum adipisci eius veritatis maiores
+							accusantium unde quae. Vel.
+							Quaerat numquam delectus sequi. Rerum iste deleniti molestiae dolorem dolore alias, odit tenetur
+							quasi nihil quia, eaque perferendis cupiditate similique magni culpa placeat molestias temporibus
+							nam laborum quos fugit laboriosam.</p>
+					</div>
+					<div class=" carousel-image">
+						<img src="@/assets/img/carousel1.svg" class="w-100" alt="..." />
+					</div>
+				</div>
+			</div>
+
+			<div class="carousel-item" data-bs-interval="2000">
+				<div class="carousel-wrapper">
+					<div class="carousel-caption">
+						<h5>Second slide label</h5>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur corrupti doloremque
+							ullam deleniti, voluptatem dolore quaerat, totam quis repellendus magnam expedita fugiat iusto
+							soluta. Perferendis quibusdam non veritatis voluptatem.
+							Quod inventore dolorum tempore magnam ex aut earum eaque deleniti alias asperiores quaerat nostrum
+							consectetur suscipit dolores, sed ullam fugit quos laborum adipisci eius veritatis maiores
+							accusantium unde quae. Vel.
+							Quaerat numquam delectus sequi. Rerum iste deleniti molestiae dolorem dolore alias, odit tenetur
+							quasi nihil quia, eaque perferendis cupiditate similique magni culpa placeat molestias temporibus
+							nam laborum quos fugit laboriosam.</p>
+					</div>
+					<div class=" carousel-image">
+						<img src="@/assets/img/carousel2.svg" class="w-100" alt="..." />
+					</div>
+				</div>
+			</div>
+
+			<div class="carousel-item">
+				<div class="carousel-wrapper">
+					<div class="carousel-caption">
+						<h5>Second slide label</h5>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur corrupti doloremque
+							ullam deleniti, voluptatem dolore quaerat, totam quis repellendus magnam expedita fugiat iusto
+							soluta. Perferendis quibusdam non veritatis voluptatem.
+							Quod inventore dolorum tempore magnam ex aut earum eaque deleniti alias asperiores quaerat nostrum
+							consectetur suscipit dolores, sed ullam fugit quos laborum adipisci eius veritatis maiores
+							accusantium unde quae. Vel.
+							Quaerat numquam delectus sequi. Rerum iste deleniti molestiae dolorem dolore alias, odit tenetur
+							quasi nihil quia, eaque perferendis cupiditate similique magni culpa placeat molestias temporibus
+							nam laborum quos fugit laboriosam.</p>
+					</div>
+					<div class=" carousel-image">
+						<img src="@/assets/img/carousel4.svg" class="w-100" alt="..." />
+					</div>
+				</div>
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+	&-indicator {}
+
+	&-wrapper {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	&-image {
+		display: flex;
+		justify-self: flex-end;
+		height: 560px;
+		width: 600px;
+
+	}
+
+	&-caption {
+		position: static;
+	}
+
+	&-control {
+		&-prev {
+			bottom: -89.5%;
+			left: 38%;
+			z-index: 999
+		}
+
+		&-next {
+			bottom: -89.5%;
+			right: 38%;
+			z-index: 999
+		}
+	}
+
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,70 +1,62 @@
-<template>
-  <div class="accordion accordion-flush" id="accordionFlushExample">
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="flush-headingOne">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-          data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-          Accordion Item #1
-        </button>
-      </h2>
-      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
-        data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-          <code>.accordion-flush</code> class. This is the first item's accordion body.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="flush-headingTwo">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-          data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-          Accordion Item #2
-        </button>
-      </h2>
-      <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
-        data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-          <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled
-          with some actual content.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="flush-headingThree">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-          data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-          Accordion Item #3
-        </button>
-      </h2>
-      <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree"
-        data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the
-          <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening
-          here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more
-          representative of how this would look in a real-world application.
-        </div>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+MyAccordion
+MyCarousel
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue'
 import MyAccordion from '@/components/MyAccordion.vue'
+import MyCarousel from '@/components/MyCarousel.vue'
 
 export default defineComponent({
-  components: {
-    MyAccordion
-  },
-  setup() {
+	components: {
+		MyAccordion,
+		MyCarousel
+	},
+	setup() {
 
 
-    return {
+		return {
 
-    }
-  }
+		}
+	}
 })
 </script>
 
 <style lang='scss'>
+.accordion {
+
+	--bs-accordion-border-color: none;
+
+	&-button {
+		color: white;
+		background-color: #515151;
+		border-radius: 5px !important;
+		border-color: #515151 !important;
+
+
+		&:not(.collapsed) {
+			color: white;
+			background-color: #515151;
+		}
+
+
+	}
+
+	&-item {
+		color: white;
+		background-color: #515151;
+		border-radius: 5px !important;
+		border-color: #515151 !important;
+
+		&:not(:first-child) {
+			margin-top: 8px;
+		}
+	}
+
+	&-flush {
+		border-radius: 5px !important;
+		border-color: #515151 !important;
+	}
+}
 </style>
