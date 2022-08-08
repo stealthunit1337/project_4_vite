505611c0bb23b4af7e8aba4f437c5f6f31ff5edf
stealthunit1337 <stealthunit123@gmail.com>
2022-08-08 16:34:37 +0300

    carousel fix+broke

--- src/components/MyCarousel.vue
+++ src/components/MyCarousel.vue
@@ -2,17 +2,9 @@
 	<div class="carousel-bg">
 		<div class="container">
 			<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-				<div class="carousel-indicators">
-					<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
-						class="active carousel-button" aria-current="true" aria-label="Slide 1"></button>
-					<button class='carousel-button' type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-						aria-label="Slide 2"></button>
-					<button class='carousel-button' type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-						aria-label="Slide 3"></button>
-				</div>
 				<div class="carousel-inner">
 
-					<div class="carousel-item active" data-bs-interval="10000">
+					<div class="carousel-item active" data-bs-interval="3000">
 						<div class="carousel-wrapper">
 							<div class="carousel-caption">
 								<h5>Second slide label</h5>
@@ -31,7 +23,7 @@
 									temporibus
 									nam laborum quos fugit laboriosam.</p>
 							</div>
-							<div class=" carousel-image">
+							<div class="carousel-image">
 								<img src="@/assets/img/carousel1.svg" class="w-100" alt="..." />
 							</div>
 						</div>
@@ -56,7 +48,7 @@
 									temporibus
 									nam laborum quos fugit laboriosam.</p>
 							</div>
-							<div class=" carousel-image">
+							<div class="carousel-image">
 								<img src="@/assets/img/carousel2.svg" class="w-100" alt="..." />
 							</div>
 						</div>
@@ -81,25 +73,63 @@
 									temporibus
 									nam laborum quos fugit laboriosam.</p>
 							</div>
-							<div class=" carousel-image">
+							<div class="carousel-image">
+								<img src="@/assets/img/carousel3.svg" class="w-100" alt="..." />
+							</div>
+						</div>
+					</div>
+
+					<div class="carousel-item">
+						<div class="carousel-wrapper">
+							<div class="carousel-caption">
+								<h5>Second slide label</h5>
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur corrupti
+									doloremque
+									ullam deleniti, voluptatem dolore quaerat, totam quis repellendus magnam expedita fugiat
+									iusto
+									soluta. Perferendis quibusdam non veritatis voluptatem.
+									Quod inventore dolorum tempore magnam ex aut earum eaque deleniti alias asperiores quaerat
+									nostrum
+									consectetur suscipit dolores, sed ullam fugit quos laborum adipisci eius veritatis maiores
+									accusantium unde quae. Vel.
+									Quaerat numquam delectus sequi. Rerum iste deleniti molestiae dolorem dolore alias, odit
+									tenetur
+									quasi nihil quia, eaque perferendis cupiditate similique magni culpa placeat molestias
+									temporibus
+									nam laborum quos fugit laboriosam.</p>
+							</div>
+							<div class="carousel-image">
 								<img src="@/assets/img/carousel4.svg" class="w-100" alt="..." />
 							</div>
 						</div>
 					</div>
-					<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-						data-bs-slide="prev">
-						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Previous</span>
-					</button>
-					<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-						data-bs-slide="next">
-						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-						<span class="visually-hidden">Next</span>
-					</button>
+					<div class="carousel-bottom">
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+							data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Previous</span>
+						</button>
+						<div class="carousel-indicators">
+							<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
+								class="active carousel-button" aria-current="true" aria-label="Slide 1"></button>
+							<button class='carousel-button' type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+								aria-label="Slide 2"></button>
+							<button class='carousel-button' type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+								aria-label="Slide 3"></button>
+							<button class='carousel-button' type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3"
+								aria-label="Slide 4"></button>
+						</div>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+							data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Next</span>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+
 </template>
 
 <script>
@@ -111,23 +141,25 @@ export default {
 <style lang="scss" scoped>
 .carousel {
 	&-bg {
-		background-color: #F6F8FA;
-
-	}
-
-
-	&-indicators {
-		bottom: 50px;
+		background-color: $background;
 
 	}
 
 	&-button {
-		border: solid 2px white;
+		border: solid 2px rgba(0, 0, 0, 0);
 		border-radius: 50%;
+		background-color: $add  !important;
+		opacity: 1;
+		margin: 0;
+
+		&:not(:last-child) {
+			margin-right: 10px;
+		}
 
 		&.active {
-			background-color: yellow;
-			border: solid 2px #515151;
+			background-color: $primary  !important;
+			border: solid 2px $add;
+
 
 		}
 	}
@@ -149,26 +181,70 @@ export default {
 		position: static;
 	}
 
+	&-bottom {
+		width: 100%;
+		position: absolute;
+		display: flex;
+		flex-direction: row;
+		bottom: 45px;
+		justify-content: center;
+	}
+
+	&-indicators {
+		position: static;
+		margin: 0;
+		align-self: center;
+	}
+
 	&-control {
 		&-prev {
-			bottom: -75%;
-			left: 36%;
+			margin-right: 24px;
+			width: 64px;
+			position: static;
 			z-index: 999;
+			opacity: 1;
 
 			&-icon {
+				background-image: url('@/assets/icons/carousel-control.svg');
 				width: 64px;
 				height: 64px;
+				filter: none;
+
+				&:hover {
+					background-image: url('@/assets/icons/carousel-control-hover.svg');
+				}
+
+				&:active {
+					background-image: url('@/assets/icons/carousel-control-active.svg');
+				}
 			}
+
 		}
 
 		&-next {
-			bottom: -75%;
-			right: 36%;
+			margin-left: 24px;
+			width: 64px;
+			position: static;
 			z-index: 999;
+			opacity: 1;
 
 			&-icon {
+				background-image: url('@/assets/icons/carousel-control.svg');
+				transform: rotate(180deg);
 				width: 64px;
 				height: 64px;
+				filter: none;
+
+				&:hover {
+					background-image: url('@/assets/icons/carousel-control-hover.svg');
+				}
+
+				&:active {
+					background-image: url('@/assets/icons/carousel-control-active.svg');
+				}
+
+
+
 			}
 		}
 	}
